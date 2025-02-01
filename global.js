92b791f05bc49d15b8c1e85e2ffd72c699cbf38c
t2liang <t2liang@ucsd.edu>
2025-01-31 21:52:55 -0800

    Update global.js

--- global.js
+++ global.js
@@ -8,10 +8,10 @@ const navLinks = $$("nav a");
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 let pages = [
-  { url: '/', title: 'Home' },
-  { url: '/projects/', title: 'Projects' },
-  { url: '/contact/', title: 'Contact'},
-  { url: '/resume', title: 'resume'}
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact'},
+  { url: 'resume', title: 'resume'}
   // add the rest of your pages here
 ];
 
