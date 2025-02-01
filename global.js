9cfd0a56a2034676189e4711eef504c7f905b665
t2liang <t2liang@ucsd.edu>
2025-02-01 14:39:29 -0800

    al

--- global.js
+++ global.js
@@ -11,7 +11,7 @@ let pages = [
   { url: '/portfolio/', title: 'Home' },
   { url: '/portfolio/projects/', title: 'Projects' },
   { url: '/portfolio/contact/', title: 'Contact'},
-  { url: '/portfolio/resume/', title: 'resume'}
+  { url: '/portfolio/resume.html', title: 'resume'}
   // add the rest of your pages here
 ];
 
