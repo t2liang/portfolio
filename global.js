f87a4d9edd4a9a449fbd22fc4b0cdda9178b3534
t2liang <t2liang@ucsd.edu>
2025-02-01 15:28:22 -0800

    4

--- global.js
+++ global.js
@@ -77,3 +77,38 @@ select.addEventListener('input', function (event) {
     // Save the user's preference to localStorage
     localStorage.colorScheme = event.target.value;
 });
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data; 
+
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    // Your code will go here
+    containerElement.innerHTML = '';
+    // Loop through each project in the array
+    project.forEach(project => {
+    // Create a new <article> element for each project
+    const article = document.createElement('article');
+
+    // Populate the <article> with project details
+    article.innerHTML = `
+      <h3>${project.title}</h3>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    // Append the <article> to the container
+    containerElement.appendChild(article);
+  });
+}
