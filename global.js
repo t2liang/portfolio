1dfcfd5678055ec998a9ed7f57d5317d638403a9
t2liang <t2liang@ucsd.edu>
2025-01-31 20:12:45 -0800

    gl

---
+++ global.js
@@ -0,0 +1,25 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const navLinks = $$("nav a");
+
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact'},
+  { url: 'resume', title: 'resume'}
+  // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
