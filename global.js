2c03c311f66ece9d0d5a5a22c0555fbf1b6b6990
t2liang <t2liang@ucsd.edu>
2025-02-01 22:48:09 -0800

    3

--- global.js
+++ global.js
@@ -122,3 +122,8 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
         containerElement.appendChild(article);
     });
 }
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+    // return statement here
+  }
--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+console.log('INDEXXX!');
+
 import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('t2liang');
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
