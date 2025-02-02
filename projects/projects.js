bd719e700b510bcc1bf7f75469fc2f3c93d7035d
t2liang <t2liang@ucsd.edu>
2025-02-01 16:50:25 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+
+console.log('projects.js loaded');
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
