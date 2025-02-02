9fd73370d93a6eff9fd22a6c67f9d3213332ee48
t2liang <t2liang@ucsd.edu>
2025-02-01 18:48:52 -0800

    a

--- projects/projects.js
+++ projects/projects.js
@@ -2,5 +2,5 @@
 console.log('projects.js loaded');
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
-const projectsContainer = document.querySelector('#projects-container');
+const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
