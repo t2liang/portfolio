d5f571e634b4e395ae1b1c917b3e0ae95fec2396
t2liang <t2liang@ucsd.edu>
2025-02-01 22:06:09 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,19 @@
 
 console.log('projects.js loaded');
 import { fetchJSON, renderProjects } from '../global.js';
-const projects = await fetchJSON('../lib/projects.json');
-const projectsContainer = document.querySelector('.projects');
-renderProjects(projects, projectsContainer, 'h2');
+
+async function loadProjects() {
+    try {
+        const projects = await fetchJSON('../lib/projects.json');
+        const projectsContainer = document.querySelector('.projects');
+
+        if (!projectsContainer) {
+            console.error("class 'projects' not found.");
+            return;
+        }
+        renderProjects(projects, projectsContainer, 'h2');
+    } catch (error) {
+        console.error("Error loading projects:", error);
+    }
+}
+loadProjects();
