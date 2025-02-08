d2ed11ba5f0c74f3657d62dfbd14d3381d217373
t2liang <t2liang@ucsd.edu>
2025-02-07 19:39:24 -0800

    1

--- projects/projects.js
+++ projects/projects.js
@@ -16,16 +16,11 @@ let total=0;
 for (let d of data) {
     total +=d;
 }
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
-let colors = ['gold', 'purple'];
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
