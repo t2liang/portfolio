44cd15884eb943d50800d44ad834e69a9c2b9645
t2liang <t2liang@ucsd.edu>
2025-02-07 20:26:05 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -4,34 +4,17 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
-
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-  ];
- 
-let sliceGenerator = d3.pie().value((d) => d.value);
+let data = [1,2,3,4,5,5];
+let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
+
 let arcs = arcData.map((d) => arcGenerator(d));
-
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
+let colors =  d3.scaleOrdinal(d3.schemeTableau10);
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
-      .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
-})
-
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-    legend.append('li')
-          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+      .attr(colors[idx]) // Fill in the attribute for fill color via indexing the colors variable
 })
