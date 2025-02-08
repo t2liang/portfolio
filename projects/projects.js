bd1c2bd3f2b87b81b72288f83ca061c4b91a1766
t2liang <t2liang@ucsd.edu>
2025-02-07 20:34:24 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -6,15 +6,23 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
-let data = [1,2,3,4,5,5];
-let sliceGenerator = d3.pie();
-let arcData = sliceGenerator(data);
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+];
 
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
+
 let colors =  d3.scaleOrdinal(d3.schemeTableau10);
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
-      .attr(colors[idx]) // Fill in the attribute for fill color via indexing the colors variable
+      .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
 })
