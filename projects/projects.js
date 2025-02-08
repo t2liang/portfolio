ddb654b7f0381171afb0d30febee7feb7a4aceaa
t2liang <t2liang@ucsd.edu>
2025-02-07 19:55:08 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -6,11 +6,7 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-    startAngle: 0,
-    endAngle: 2 * Math.PI,
-});
+
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 let data = [
     { value: 1, label: 'apples' },
@@ -20,21 +16,18 @@ let data = [
     { value: 5, label: 'limes' },
     { value: 5, label: 'cherries' },
   ];
-  
-let total=0;
-for (let d of data) {
-    total +=d;
-}
+ 
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
 arcs.forEach((arc, idx) => {
     d3.select('svg')
       .append('path')
       .attr('d', arc)
-      .attr('fill', colors[idx]) // Fill in the attribute for fill color via indexing the colors variable
+      .attr('fill', colors(idx)) // Fill in the attribute for fill color via indexing the colors variable
 })
 
 let legend = d3.select('.legend');
