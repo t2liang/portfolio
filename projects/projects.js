e493e16997b23448208b3fa10d0cbd8598d1ba20
t2liang <t2liang@ucsd.edu>
2025-02-07 21:41:54 -0800

    .

--- projects/projects.js
+++ projects/projects.js
@@ -32,6 +32,7 @@ arcs.forEach((arc, idx) => {
 let legend = d3.select('.legend');
 data.forEach((d, idx) => {
     legend.append('li')
-          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
-          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+        .attr('class', 'legend-item')
+        .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+        .text(d.label); // set the inner html of <li>
 })
