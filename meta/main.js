26bd28e91120e99279e286cccccd47d106f79fee
t2liang <t2liang@ucsd.edu>
2025-02-14 00:26:43 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -3,13 +3,54 @@ let data = [];
 
 
 async function loadData() {
-  data = await d3.csv('loc.csv');
-  console.log(data);
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+    
+  }));
+  processCommits();
+  console.log(commits);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
+
+function processCommits() {
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: `https://github.com/YOUR_REPO/commit/${commit}`,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      // Store lines but keep it hidden in console logs
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        writable: false,
+        enumerable: false,  // Prevent cluttering console logs
+        configurable: false
+      });
+
+      return ret;
+    });
+}
+
+
 
 function displayStats() {
   // Process commit data
