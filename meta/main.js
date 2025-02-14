f8a697f8c2dd6ee769453c43635107216b77ba4e
t2liang <t2liang@ucsd.edu>
2025-02-13 23:55:32 -0800

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -1,56 +1,15 @@
-console.log("main.js is successfully loaded!");
+
 let data = [];
 
+
 async function loadData() {
-  data = await d3.csv('loc.csv', (row) => ({
-    ...row,
-    line: Number(row.line), // or just +row.line
-    depth: Number(row.depth),
-    length: Number(row.length),
-    date: new Date(row.date + 'T00:00' + row.timezone),
-    datetime: new Date(row.datetime),
-  }));
-  displayStats();
-  console.log(commits);
+  data = await d3.csv('loc.csv');
+  console.log(data);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
-
-function processCommits() {
-  commits = d3
-    .groups(data, (d) => d.commit)
-    .map(([commit, lines]) => {
-      let first = lines[0];
-      let { author, date, time, timezone, datetime } = first;
-      let ret = {
-        id: commit,
-        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
-        author,
-        date,
-        time,
-        timezone,
-        datetime,
-        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-        totalLines: lines.length,
-      };
-
-      Object.defineProperty(ret, 'lines', {
-        value: lines,
-        enumerable: false, // Hides the property when logging the object
-        writable: false, // Prevents modification
-        configurable: false, // Prevents deletion or redefinition
-      });
-
-      return ret;
-    });
-}
-async function loadData() {
-  // original function as before
-  processCommits();
-  console.log(commits);
-}
 
 function displayStats() {
   // Process commit data
